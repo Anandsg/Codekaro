c3b4b7cf42b424fc912251fbb962dc7f4fa60a3f
Anandagadugin <gaduginanand984@gmail.com>
2023-08-09 09:56:31 +0530

    Assignment 2 object operations final solution

--- Assignment2-ObjectOperations.js
+++ Assignment2-ObjectOperations.js
@@ -1,2 +1,24 @@
 // 1. Create an empty object called "person".
 var person = {};
+
+// 2. Add properties to the "person" object: name, age, city.
+person.name = "John";
+person.age = 30;
+person.city = "New York";
+console.log(person)
+
+// 3. Remove the "age" property from the "person" object.
+delete person.age
+console.log(person)
+
+// 4. Add a new property "job" with the value "Engineer" to the "person" object.
+person.job = "Engineer";
+console.log(person)
+
+
+// 5. Update the "city" property of the "person" object to "San Francisco".
+person.city = "San Francisco";
+console.log(person)
+
+// 6. Print the final "person" object.
+console.log("Final person object:", person);
