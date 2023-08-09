21361bdf0606e2771868ef21b0cc4bd9f42e07a2
Anandagadugin <gaduginanand984@gmail.com>
2023-08-09 09:03:20 +0530

    Assignment 1 array operations solution

---
+++ Assignment1-ArrayOperations.js
@@ -0,0 +1,27 @@
+// 1. Creating an empty array called "fruits".
+var fruits = [];
+
+// 2. Adding the fruits to the "fruits" array: "apple", "banana", "orange".
+fruits = ['apple', 'banana', 'orange'];
+console.log(fruits);
+
+// 3. Remove the first fruit from the array.
+fruits.shift();
+console.log(fruits);
+
+// 4. Add "grape" to the end of the array.
+fruits.push("grape");
+console.log(fruits)
+
+// 5. Update the second fruit in the array to "pear".
+fruits[1] = "pear";
+console.log(fruits)
+
+// 6. Print the final "fruits" array.
+console.log("Final fruits array:", fruits);
+
+
+
+
+
+
